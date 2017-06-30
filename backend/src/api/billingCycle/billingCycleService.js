c90fd5f0b6c19a95973ea11a32a974040fe21d71
Wagner Almeida <wdmeida@gmail.com>
2017-06-30 18:25:25 -0300

    Created aggregation with the sum of debts and credits.

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -3,4 +3,32 @@ const BillingCycle = require('./billingCycle')
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({ new: true, runValidators: true })
 
+// Count items
+BillingCycle.route('count',  (req, res, next) => {
+  BillingCycle.count((error, value) => {
+    if (error) {
+      res.status(500).json({ errors: [error] })
+    } else {
+      res.json({value})
+    }
+  })
+})
+
+// Summary items
+BillingCycle.route('summary', (req, res, next) => {
+  BillingCycle.aggregate({
+    $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}}
+  }, {
+    $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
+  }, {
+    $project: {_id: 0, credit: 1, debt: 1}
+  }, (error, result) => {
+    if (error) {
+      res.status(500).json({errors: [error]})
+    } else {
+      res.json(result[0] || { credit: 0, debt: 0 })
+    }
+  })
+})
+
 module.exports = BillingCycle
