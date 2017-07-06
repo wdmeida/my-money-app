9d4f12f886c8a6be863edd803adb14c9fdee3e71
Wagner Almeida <wdmeida@gmail.com>
2017-07-06 20:40:44 -0300

    Including the form and functionality of adding a new payment cycle.

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,4 +1,8 @@
 import axios from 'axios'
+import { toastr } from 'react-redux-toastr'
+import { reset as resetForm } from 'redux-form'
+import { showTabs, selectTab } from '../common/tab/tabActions'
+
 const BASE_URL = 'http://localhost:3003/api'
 
 export function getList() {
@@ -10,8 +14,19 @@ export function getList() {
 }
 
 export function create(values) {
-  axios.post(`${BASE_URL}/billingCycles`, values)
-  return {
-    type: 'TEMP'
+  return dispatch => {
+    axios.post(`${BASE_URL}/billingCycles`, values)
+        .then(resp => {
+          toastr.success('Sucesso', 'Operação realizada com sucesso.')
+          dispatch([
+            resetForm('billingCycleForm'),
+            getList(),
+            selectTab('tabList'),
+            showTabs('tabList', 'tabCreate')
+          ])
+        })
+        .catch(e => {
+          e.response.data.errors.forEach(error => toastr.error('Erro', error))
+        })
   }
 }
