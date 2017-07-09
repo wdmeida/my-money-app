e7ec43ce71fab04799799619f1208fef379d3e20
Wagner Almeida <wdmeida@gmail.com>
2017-07-09 17:00:18 -0300

    Finishing initial crud of billing cycles.

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -14,7 +14,7 @@ import List from './billingCycleList'
 import Form from './billingCycleForm'
 
 import { selectTab, showTabs } from '../common/tab/tabActions'
-import { create } from './billingCycleActions'
+import { create, update, remove } from './billingCycleActions'
 
 class BillingCycle extends Component {
 
@@ -40,12 +40,24 @@ class BillingCycle extends Component {
                 <List />
               </TabContent>
               <TabContent id='tabCreate'>
-                <Form onSubmit={this.props.create} />
+                <Form 
+                  onSubmit={this.props.create}
+                  submitLabel='Incluir'
+                  submitClass='primary' />
               </TabContent>
               <TabContent id='tabUpdate'>
-                <Form />
+                <Form 
+                  onSubmit={this.props.update}
+                  submitLabel='Alterar'
+                  submitClass='info' />
               </TabContent>
-              <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
+              <TabContent id='tabDelete'>
+                <Form 
+                  onSubmit={this.props.remove} 
+                  readOnly={true}
+                  submitLabel='Excluir'
+                  submitClass='danger' />
+              </TabContent>
             </TabsContent>
           </Tabs>
         </Content>
@@ -55,6 +67,6 @@ class BillingCycle extends Component {
 }
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-  create, selectTab, showTabs
+  create, selectTab, showTabs, update, remove
 }, dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
--- frontend/src/billingCycle/billingCycleList.jsx
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
-import { getList, showUpdate } from './billingCycleActions'
+import { getList, showUpdate, showDelete } from './billingCycleActions'
 
 class BillingCycleList extends Component {
 
@@ -22,6 +22,9 @@ class BillingCycleList extends Component {
           <button className='btn btn-warning' onClick={() => this.props.showUpdate(bc)}>
             <i className='fa fa-pencil'></i>
           </button>
+          <button className='btn btn-danger' onClick={() => this.props.showDelete(bc)}>
+            <i className='fa fa-trash-o'></i>
+          </button>
         </td>
       </tr>
     ))
@@ -36,7 +39,7 @@ class BillingCycleList extends Component {
               <th>Nome</th>
               <th>Mês</th>
               <th>Ano</th>
-              <th>Ações</th>
+              <th className='table-actions'>Ações</th>
             </tr>
           </thead>
           <tbody>
@@ -49,5 +52,5 @@ class BillingCycleList extends Component {
 }
 
 const mapStateToProps = state => ({list: state.billingCycle.list})
-const mapDispatchToProps = dispatch => bindActionCreators({getList, showUpdate}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({getList, showUpdate, showDelete}, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleList)
