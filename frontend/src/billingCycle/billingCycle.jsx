7e00f7459fd4e9a448d7db4c8cf1d372012e78f0
Wagner Almeida <wdmeida@gmail.com>
2017-07-09 21:17:05 -0300

    Added insertion of credits and debits for each payment cycle and refactoring the application menu.

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -13,14 +13,12 @@ import TabContent from '../common/tab/tabContent'
 import List from './billingCycleList'
 import Form from './billingCycleForm'
 
-import { selectTab, showTabs } from '../common/tab/tabActions'
-import { create, update, remove } from './billingCycleActions'
+import { init, create, update, remove } from './billingCycleActions'
 
 class BillingCycle extends Component {
 
   componentWillMount() {
-    this.props.selectTab('tabList')
-    this.props.showTabs('tabList', 'tabCreate')
+    this.props.init()
   }
 
   render() {
@@ -67,6 +65,6 @@ class BillingCycle extends Component {
 }
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-  create, selectTab, showTabs, update, remove
+  init, create, update, remove
 }, dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
